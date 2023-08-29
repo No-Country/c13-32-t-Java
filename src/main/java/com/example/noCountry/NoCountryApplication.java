feb5c728048ebfc9db98e3bee3e34d9690fb191b
naty <natymayorn@gmail.com>
2023-08-29 15:57:18 -0500

    create user: service and controller

--- src/main/java/com/example/noCountry/NoCountryApplication.java
+++ src/main/java/com/example/noCountry/NoCountryApplication.java
@@ -2,6 +2,9 @@ package com.example.noCountry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NoCountryApplication {
@@ -10,4 +13,8 @@ public class NoCountryApplication {
 		SpringApplication.run(NoCountryApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
---
+++ src/main/java/com/example/noCountry/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.example.noCountry.controllers;
+
+import com.example.noCountry.Entity.User;
+import com.example.noCountry.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //add user
+    @PostMapping("/create")
+    public ResponseEntity<String> addUser(@RequestBody User user){
+        User createdUser = userService.createUser(user.getEmail(), user.getPassword(), user.getRole());
+        if (createdUser != null) {
+            return ResponseEntity.ok("Usuario creado");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el usuario");
+        }
+    }
+
+
+
+
+
+
+}
